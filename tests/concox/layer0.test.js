8ca506bf42a422799ffa77ec46f51504bb4fdf22
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-16 04:41:11 -0300

    Create layer0.test.js

---
+++ tests/concox/layer0.test.js
@@ -0,0 +1,14 @@
+const { randomBytes } = require("crypto");
+const { addLayer0, removeLayer0 } = require("../../src/concox/layer0");
+//65534
+describe("layer0Pack", () => {
+  test("Test Package", () => {
+    let i = 0;
+    while (i < 65534) {
+      let pkg0 = randomBytes(i);
+
+      expect(removeLayer0(addLayer0(pkg0))).toStrictEqual(pkg0);
+      i = i == 0 ? 1 : i * 2;
+    }
+  });
+});
