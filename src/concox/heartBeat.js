a19f07c6c158cb31355f00b87271b598f70830b9
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-16 05:38:28 -0300

    Create heartBeat.js

---
+++ src/concox/heartBeat.js
@@ -0,0 +1,17 @@
+const { getFlagFromByte, parserPackageComponents } = require("./common");
+module.exports.parseHeartbeat = function (buffer) {
+  let [statusByte, voltageByte, signalByte] = parserPackageComponents(
+    buffer,
+    [1, 1, 1]
+  );
+
+  return {
+    batteryVoltage: voltageByte.readUInt8(),
+    cellularSignal: signalByte.readUInt8(),
+    defense: getFlagFromByte(statusByte, 0),
+    acc: getFlagFromByte(statusByte, 1),
+    power: getFlagFromByte(statusByte, 2),
+    fixed: getFlagFromByte(statusByte, 6),
+    cut: getFlagFromByte(statusByte, 7),
+  };
+};
