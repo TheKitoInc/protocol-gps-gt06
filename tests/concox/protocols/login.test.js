72efdd2de72deee81b920f3243494864473d93c7
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-17 16:43:01 -0300

    Create login.test.js

---
+++ tests/concox/protocols/login.test.js
@@ -0,0 +1,22 @@
+const { parse } = require("../../../src/concox/protocols/protocol-01");
+
+describe("login", () => {
+  let testData = Buffer.from([
+    0x07, 0x52, 0x53, 0x36, 0x78, 0x90, 0x02, 0x42, 0x70, 0x00, 0x32, 0x01,
+  ]);
+  let testData2 = Buffer.from([
+    0x07, 0x52, 0x53, 0x36, 0x78, 0x90, 0x02, 0x42, 0x70, 0x00, 0x4d, 0xdd,
+  ]);
+  test("Test login package MAC", () => {
+    expect(parse(testData).imei).toStrictEqual("0752533678900242");
+  });
+  test("Test login package TYPE", () => {
+    expect(parse(testData).type).toStrictEqual(28672);
+  });
+  test("Test login package TIMEZONE +", () => {
+    expect(parse(testData).timeZone).toStrictEqual(480);
+  });
+  test("Test login package TIMEZONE -", () => {
+    expect(parse(testData2).timeZone).toStrictEqual(-765);
+  });
+});
