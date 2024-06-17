5298d445792de890480d9629b6be7c8164b5e9c4
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-17 02:13:19 -0300

    Update tables.js

--- src/concox/tables.js
+++ src/concox/tables.js
@@ -1,34 +1,37 @@
 const { getFlagFromByte, splitBufferBytes } = require("./common");
 
-module.exports.parseStatusByte = function (byte) {
-    return {
-      defense: getFlagFromByte(byte, 0),
-      acc: getFlagFromByte(byte, 1),
-      power: getFlagFromByte(byte, 2),
-  
-      lbat:
-        getFlagFromByte(byte, 3) &&
-        getFlagFromByte(byte, 4),
-      powercut:
-        !getFlagFromByte(byte, 3) &&
-        getFlagFromByte(byte, 4),
-      vibrating:
-        getFlagFromByte(byte, 3) &&
-        !getFlagFromByte(byte, 4),
-  
-      sos: getFlagFromByte(byte, 5),
-      fixed: getFlagFromByte(byte, 6),
-      cut: getFlagFromByte(byte, 7),
-    };
-  };
+module.exports.parseStatusByte = function (buffer) {
+  byte = buffer[0];
+  return {
+    defense: getFlagFromByte(byte, 0),
+    acc: getFlagFromByte(byte, 1),
+    power: getFlagFromByte(byte, 2),
 
-  module.exports.parseCourse = function (buffer) {
-    let [upperByte, lowerByte] = splitBufferBytes(buffer, 6);
-    return {
-      realTime: getFlagFromByte(upperByte, 5),
-      fix: getFlagFromByte(upperByte, 4),
-      east: getFlagFromByte(upperByte, 3),
-      north: getFlagFromByte(upperByte, 2),
-      course: lowerByte.readUInt16BE(),
-    };
+    lbat: getFlagFromByte(byte, 3) && getFlagFromByte(byte, 4),
+    powercut: !getFlagFromByte(byte, 3) && getFlagFromByte(byte, 4),
+    vibrating: getFlagFromByte(byte, 3) && !getFlagFromByte(byte, 4),
+
+    sos: getFlagFromByte(byte, 5),
+    fixed: getFlagFromByte(byte, 6),
+    cut: getFlagFromByte(byte, 7),
   };
+};
+
+module.exports.parseCourse = function (buffer) {
+  let [upperByte, lowerByte] = splitBufferBytes(buffer, 6);
+  upperByte = upperByte[0];
+
+  let east_west = getFlagFromByte(upperByte, 3);
+  let south_north = getFlagFromByte(upperByte, 2);
+  return {
+    realTime: getFlagFromByte(upperByte, 5),
+    fixed: getFlagFromByte(upperByte, 4),
+
+    south: !south_north,
+    north: south_north,
+    east: !east_west,
+    west: east_west,
+    
+    course: lowerByte.readUInt16BE(),
+  };
+};
