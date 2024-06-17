a51fe7a42167ae774724dff0045b301410b93a5a
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-17 16:13:03 -0300

    Update dateTime.js

--- src/concox/dateTime.js
+++ src/concox/dateTime.js
@@ -15,3 +15,18 @@ module.exports.parse = function (buffer) {
     seconds: seconds.readUInt8(),
   };
 };
+
+module.exports.response = function () {
+  let buffer = Buffer.alloc(6);
+
+  let date = new Date();
+
+  buffer.writeUint8(date.getUTCFullYear() - 2000, 0);
+  buffer.writeUint8(date.getUTCMonth(), 1);
+  buffer.writeUint8(date.getUTCDay(), 2);
+  buffer.writeUint8(date.getUTCHours(), 3);
+  buffer.writeUint8(date.getUTCMinutes(), 4);
+  buffer.writeUint8(date.getUTCSeconds(), 5);
+
+  return buffer;
+};
