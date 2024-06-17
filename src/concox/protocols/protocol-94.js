51205da397a9204247473a115e4a7e173123b748
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-17 13:39:42 -0300

    Create protocol-94.js

---
+++ src/concox/protocols/protocol-94.js
@@ -0,0 +1,16 @@
+//Protocol Transfer
+
+const { parserPackageComponents } = require("../common");
+
+module.exports.parse = function (buffer) {
+  let [idTransfer, data] = parserPackageComponents(buffer, [1],true);
+
+  return {
+    typeName:"Transfer",
+    typeId: "04",
+    idTransfer:idTransfer,
+    payload:data
+
+  };
+};
+
