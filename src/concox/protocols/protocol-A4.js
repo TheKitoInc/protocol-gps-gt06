ebf515a396c09a7a8cf9693b84020e9f61f5c68d
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-17 16:14:38 -0300

    Update protocol-A4.js

--- src/concox/protocols/protocol-A4.js
+++ src/concox/protocols/protocol-A4.js
@@ -1,7 +1,7 @@
 //Protocol Multi-fence Alarm
 
 const { parserPackageComponents } = require("../common");
-const dateTime = require("../dateTime");
+const dateTime = require("../tables/dateTime");
 const location = require("../tables/location");
 const cell = require("../cell");
 const status = require("../tables/status");
