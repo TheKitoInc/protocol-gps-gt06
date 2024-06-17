6dc390e70c757339bb220c19e9f35d5adf1e3da2
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-17 02:44:01 -0300

    Update location.js

--- src/concox/location.js
+++ src/concox/location.js
@@ -1,17 +1,17 @@
 const { parserPackageComponents, getFlagFromByte } = require("./common");
+const { parseCourse } = require("./tables");
 
 module.exports.parse = function (buffer) {
-    let [satellites, latitude, longitude, speed, course] =
-      parserPackageComponents(buffer, [ 1, 4, 4, 1, 2]);
-  
-    return {
-      satellites: satellites.readUInt8(),
-      latitude:
-        (latitude.readUInt32BE() / 1800000) *
-        (getFlagFromByte(course, 3) ? -1 : 1),
-      longitude:
-        (longitude.readUInt32BE() / 1800000) *
-        (getFlagFromByte(course, 2) ? 1 : -1),
-      speed: speed.readUInt8(),
-    };
-  }
+  let [satellites, latitude, longitude, speed, bufferCourse] =
+    parserPackageComponents(buffer, [1, 4, 4, 1, 2]);
+
+  let { course, east, fixed, north, realTime } = parseCourse(bufferCourse);
+  console.log(parseCourse(bufferCourse))
+  return {
+    satellites: satellites.readUInt8(),
+    latitude: (latitude.readUInt32BE() / 1800000) * (north ? 1 : -1),
+    longitude: (longitude.readUInt32BE() / 1800000) * (east ? 1 : -1),
+    course: course,
+    speed: speed.readUInt8(),
+  };
+};
