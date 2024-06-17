6d697ad3e06412848a8818b90ac486742c78423c
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-17 02:22:07 -0300

    Update common.js

--- src/concox/common.js
+++ src/concox/common.js
@@ -1,5 +1,5 @@
 module.exports.getFlagFromByte = function (byte, index) {
-  return byte[0].toString(2).padStart(8, 0)[7 - index] == 1;
+  return byte.toString(2).padStart(8, 0)[7-index] == 1;
 };
 
 module.exports.parserPackageComponents = function (
