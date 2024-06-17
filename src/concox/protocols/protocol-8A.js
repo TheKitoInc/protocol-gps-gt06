a0e6f547f2d85f5da9aad575632a0125de909cba
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-17 16:42:40 -0300

    Update protocol-8A.js

--- src/concox/protocols/protocol-8A.js
+++ src/concox/protocols/protocol-8A.js
@@ -5,5 +5,14 @@ const dateTime = require("../tables/dateTime");
 module.exports.parse = function (buffer) {};
 
 module.exports.response = function () {
-  return dateTime.response();
+  let date = new Date();
+
+  return dateTime.response(
+    date.getUTCFullYear() - 2000,
+    date.getUTCMonth(),
+    date.getUTCDay(),
+    date.getUTCHours(),
+    date.getUTCMinutes(),
+    date.getUTCSeconds()
+  );
 };
