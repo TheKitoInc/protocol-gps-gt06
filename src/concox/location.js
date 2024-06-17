0d4a21e3ec81b25027c1e884418c2b4ac4d9f8c2
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-17 02:58:55 -0300

    Update location.js

--- src/concox/location.js
+++ src/concox/location.js
@@ -5,15 +5,17 @@ module.exports.parse = function (buffer) {
   let [satellites, latitude, longitude, speed, bufferCourse] =
     parserPackageComponents(buffer, [1, 4, 4, 1, 2]);
 
-    let courseObject =  courseParser.parse(bufferCourse);
+  let courseObject = courseParser.parse(bufferCourse);
 
   return {
     satellites: satellites.readUInt8(),
-    latitude: (latitude.readUInt32BE() / 1800000) * (courseObject.north ? 1 : -1),
-    longitude: (longitude.readUInt32BE() / 1800000) * (courseObject.east ? 1 : -1),  
+    latitude:
+      (latitude.readUInt32BE() / 1800000) * (courseObject.north ? 1 : -1),
+    longitude:
+      (longitude.readUInt32BE() / 1800000) * (courseObject.east ? 1 : -1),
     speed: speed.readUInt8(),
-    fixed : courseObject.fixed,
-    realTime:courseObject.realTime,
+    fixed: courseObject.fixed,
+    realTime: courseObject.realTime,
     course: courseObject.course,
   };
 };
