fd838f482ccce47045be6c339a6eb309ca551f69
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-17 03:02:09 -0300

    Create location.js

---
+++ src/concox/protocols/location.js
@@ -0,0 +1,21 @@
+const { parserPackageComponents, getFlagFromByte } = require("../common");
+const courseParser = require("../tables/course");
+
+module.exports.parse = function (buffer) {
+  let [satellites, latitude, longitude, speed, bufferCourse] =
+    parserPackageComponents(buffer, [1, 4, 4, 1, 2]);
+
+  let courseObject = courseParser.parse(bufferCourse);
+
+  return {
+    satellites: satellites.readUInt8(),
+    latitude:
+      (latitude.readUInt32BE() / 1800000) * (courseObject.north ? 1 : -1),
+    longitude:
+      (longitude.readUInt32BE() / 1800000) * (courseObject.east ? 1 : -1),
+    speed: speed.readUInt8(),
+    fixed: courseObject.fixed,
+    realTime: courseObject.realTime,
+    course: courseObject.course,
+  };
+};
