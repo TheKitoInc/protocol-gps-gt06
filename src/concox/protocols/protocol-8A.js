17492193d5f170385f785675e188963198bf88d1
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-17 16:13:12 -0300

    Create protocol-8A.js

---
+++ src/concox/protocols/protocol-8A.js
@@ -0,0 +1,9 @@
+//Protocol Ask Time
+
+const dateTime = require("../dateTime");
+
+module.exports.parse = function (buffer) {};
+
+module.exports.response = function () {
+  return dateTime.response();
+};
