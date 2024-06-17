692cc12a7923fc7bafcd415a1c088543f8b7dbc8
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-17 15:54:03 -0300

    Update index.js

--- src/concox/protocols/index.js
+++ src/concox/protocols/index.js
@@ -20,19 +20,37 @@ module.exports.parse = function (buffer) {
   buffer = buffer.subarray(0, -2);
 
   let object = null;
-  if (protocolLogin.equals(protocol)) object = login.parse(package.buffer);
-  else if (protocolHeartbeat.equals(protocol)) object = heartbeat.parse(buffer);
-  else if (protocolAlarm.equals(protocol)) object = alarm.parse(buffer);
-  else if (protocolLocation.equals(protocol)) object = location.parse(buffer);
-  else if (protocolTransfer.equals(protocol)) object = transfer.parse(buffer);
-  else if (protocolMultiBase.equals(protocol)) object = multibase.parse(buffer);
-  else {
+  let response = null;
+
+  if (protocolLogin.equals(protocol)) {
+    object = login.parse(package.buffer);
+    response = login.response();
+  } else if (protocolHeartbeat.equals(protocol)) {
+    object = heartbeat.parse(buffer);
+    response = heartbeat.response();
+  } else if (protocolAlarm.equals(protocol)) {
+    object = alarm.parse(buffer);
+    response = alarm.response(buffer);
+  } else if (protocolLocation.equals(protocol)) {
+    object = location.parse(buffer);
+    response = location.response(buffer);
+  } else if (protocolTransfer.equals(protocol)) {
+    object = transfer.parse(buffer);
+    response = transfer.response(buffer);
+  } else if (protocolMultiBase.equals(protocol)) {
+    object = multibase.parse(buffer);
+    response = multibase.response(buffer);
+  } else {
     console.warn(protocol);
   }
+
+  if (response !== null)
+    response = Buffer.concat([protocol, response, sequence]);
 
   return {
     protocol: protocol,
     sequence: sequence,
     object: object,
+    response: response,
   };
 };
