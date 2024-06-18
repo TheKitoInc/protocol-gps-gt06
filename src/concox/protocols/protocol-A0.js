003c1eee5006bf1290438f2963d7876a68e3dcc1
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-18 19:49:56 -0300

    Update protocol-A0.js

--- src/concox/protocols/protocol-A0.js
+++ src/concox/protocols/protocol-A0.js
@@ -1,9 +1,11 @@
 //Protocol Location
+"use strict";
 
 const { parserPackageComponents } = require("../common");
 const dateTime = require("../tables/dateTime");
 const location = require("../tables/location");
-const cell = require("../cell");
+const cell = require("../tables/cell");
+const extend = require("extend");
 
 module.exports.parse = function (buffer) {
   let [dateTimeBuffer, locationBuffer, data] = parserPackageComponents(
@@ -19,18 +21,30 @@ module.exports.parse = function (buffer) {
     [1, 1, 1, 4]
   );
 
-  return {
-    typeName: "Location",
-    typeId: "A0",
+  let object = {
+    event: {
+      uploadTrigger: uploadmode.readUInt8(),
+    },
 
-    timeStamp: dateTime.parse(dateTimeBuffer),
-    location: location.parse(locationBuffer),
-    cell: cellObject,
-    reupload: reupload,
-    uploadmode: uploadmode,
-    acc: acc,
-    mileage: mileage.readUInt32BE(),
+    power: {
+      acc: acc[0] > 0,
+    },
+
+    gps: {
+      realTime: reupload[0] == 0,
+      mileage: mileage.readUInt32BE(),
+    },
+
+    raw: {
+      locationProtocol: [...reupload, ...uploadmode, ...acc, ...mileage],
+    },
   };
+
+  extend(true, object, dateTime.parse(dateTimeBuffer));
+  extend(true, object, location.parse(locationBuffer));
+  extend(true, object, cellObject);
+
+  return object;
 };
 
 module.exports.response = function () {
