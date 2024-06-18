e46e6f98bf7009ad82adc10f64f2402febd152c4
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-18 18:30:26 -0300

    Update course.js

--- src/concox/tables/course.js
+++ src/concox/tables/course.js
@@ -1,22 +1,28 @@
-const { getFlagFromByte, splitBufferBytes } = require("./../common");
+"use strict";
 
+const { getFlagFromByte } = require("./../common");
 
 module.exports.parse = function (buffer) {
-  let [upperByte, lowerByte] = splitBufferBytes(buffer, 6);
+  let upperByte = buffer[0];
+  let lowerByte = buffer[1];
 
-  upperByte = upperByte[0];
+  let course = Buffer.from([upperByte & 0x03, lowerByte]);
+
   let east_west = getFlagFromByte(upperByte, 3);
   let south_north = getFlagFromByte(upperByte, 2);
 
   return {
-    realTime: getFlagFromByte(upperByte, 5),
-    fixed: getFlagFromByte(upperByte, 4),
-
-    south: !south_north,
-    north: south_north,
-    east: !east_west,
-    west: east_west,
-
-    course: lowerByte.readUInt16BE(),
+    gps: {
+      realTime: getFlagFromByte(upperByte, 5),
+      fixed: getFlagFromByte(upperByte, 4),
+      course: course.readUInt16BE(),
+      region: {
+        west: east_west,
+        east: !east_west,
+        north: south_north,
+        south: !south_north,
+      },
+    },
+    rawCourse: [upperByte, lowerByte],
   };
 };
