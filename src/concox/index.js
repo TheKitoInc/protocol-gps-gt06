41b6d4214ea80834706712cc5156356ab98977d7
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-18 21:27:50 -0300

    Update index.js

--- src/concox/index.js
+++ src/concox/index.js
@@ -1,8 +1,14 @@
 "use strict";
 
-const { removeLayer0 } = require("./layer0");
+const { removeLayer0, addLayer0 } = require("./layer0");
 const protocols = require("./protocols");
 
 module.exports.parse = function (buffer) {
   return protocols.parse(removeLayer0(buffer));
+};
+
+module.exports.response = function (protocol, sequence, buffer = null) {
+  if (!buffer) buffer = Buffer.alloc(0);
+
+  return addLayer0(protocols.response(protocol, sequence, buffer));
 };
