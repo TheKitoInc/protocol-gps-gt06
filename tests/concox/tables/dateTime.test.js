6f5751d8fd6ed11700f0e4c0a449a57f2f5f7e7e
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-18 21:48:35 -0300

    Update dateTime.test.js

--- tests/concox/tables/dateTime.test.js
+++ tests/concox/tables/dateTime.test.js
@@ -4,22 +4,22 @@ describe("dateTime", () => {
   let testData = Buffer.from([0x0f, 0x0c, 0x1d, 0x02, 0x33, 0x05]);
 
   test("Test dateTime Table year", () => {
-    expect(parse(testData).year).toStrictEqual(2015);
+    expect(parse(testData).timeStamp.year).toStrictEqual(2015);
   });
   test("Test dateTime Table month", () => {
-    expect(parse(testData).month).toStrictEqual(12);
+    expect(parse(testData).timeStamp.month).toStrictEqual(12);
   });
   test("Test dateTime Table day +", () => {
-    expect(parse(testData).day).toStrictEqual(29);
+    expect(parse(testData).timeStamp.day).toStrictEqual(29);
   });
   test("Test dateTime Table hours", () => {
-    expect(parse(testData).hours).toStrictEqual(2);
+    expect(parse(testData).timeStamp.hours).toStrictEqual(2);
   });
   test("Test dateTime Table minutes", () => {
-    expect(parse(testData).minutes).toStrictEqual(51);
+    expect(parse(testData).timeStamp.minutes).toStrictEqual(51);
   });
   test("Test dateTime Table seconds", () => {
-    expect(parse(testData).seconds).toStrictEqual(5);
+    expect(parse(testData).timeStamp.seconds).toStrictEqual(5);
   });
 
   test("Test dateTime Table response", () => {
