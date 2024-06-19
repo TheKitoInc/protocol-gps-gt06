79d41e3caa857bd7b514a6eab9b8f7b9323e4127
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2024-06-18 21:00:37 -0300

    Update status.js

--- src/concox/tables/status.js
+++ src/concox/tables/status.js
@@ -6,16 +6,22 @@ module.exports.parse = function (buffer) {
   let byte = buffer[0];
 
   return {
-    defense: getFlagFromByte(byte, 0),
-    acc: getFlagFromByte(byte, 1),
-    power: getFlagFromByte(byte, 2),
+    power: {
+      acc: getFlagFromByte(byte, 1),
+      connected: getFlagFromByte(byte, 2),
+    },
 
-    lowBattery: getFlagFromByte(byte, 3) && getFlagFromByte(byte, 4),
-    powerLost: !getFlagFromByte(byte, 3) && getFlagFromByte(byte, 4),
-    vibrating: getFlagFromByte(byte, 3) && !getFlagFromByte(byte, 4),
+    alarm: {
+      sos: getFlagFromByte(byte, 5),
+      vibration: getFlagFromByte(byte, 3) && !getFlagFromByte(byte, 4),
+      lowBattery: getFlagFromByte(byte, 3) && getFlagFromByte(byte, 4),
+      powerLost: !getFlagFromByte(byte, 3) && getFlagFromByte(byte, 4),
+    },
 
-    sos: getFlagFromByte(byte, 5),
-    stoped: getFlagFromByte(byte, 7),
+    status: {
+      defense: getFlagFromByte(byte, 0),
+      stoped: getFlagFromByte(byte, 7),
+    },
 
     gps: {
       fixed: getFlagFromByte(byte, 6),
