178e43fb9399d76446692cd670ba395211628572
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2024-06-22 17:35:53 -0300

    Update protocol-01.js

--- src/protocols/protocol-01.js
+++ src/protocols/protocol-01.js
@@ -1,7 +1,7 @@
 //Protocol Login
 "use strict";
 
-const { parserPackageComponents } = require("../common");
+const { parserPackageComponents, getFlagFromByte } = require("../common");
 
 module.exports.parse = function (buffer) {
   const [imei, type, tz] = parserPackageComponents(buffer, [8, 2, 2]);
@@ -21,16 +21,13 @@ module.exports.parse = function (buffer) {
 };
 
 function getTimeZone(buffer) {
-  buffer = buffer.toString("hex");
-
-  const timeBytes = Buffer.from("0" + buffer.slice(0, 3), "hex");
-  const timeInt = timeBytes.readUInt16BE();
+  const flagGMT = getFlagFromByte(buffer[1], 3);
+  
+  const timeInt = Buffer.from([buffer[0], buffer[1] & 0b11110000]).readUInt16BE()>>4
   const timeString = timeInt.toString();
-  const timeMinutes = parseInt(timeString.slice(0, -2)) * 60 + parseInt(timeString.slice(-2));
-
-  const byteFlags = Buffer.from("0" + buffer.slice(3, 4), "hex");
-
-  return byteFlags.readUInt8() > 7 ? -timeMinutes : timeMinutes;
+  const timeMinutes = parseInt(timeString.slice(0, -2),10) * 60 + parseInt(timeString.slice(-2),10);
+ 
+  return flagGMT ? -timeMinutes : timeMinutes;
 }
 
 module.exports.response = function () {
