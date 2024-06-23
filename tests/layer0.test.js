906079d0d7dcf0fe1589c1660d1bbb07c6e758cb
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2024-06-22 21:49:16 -0300

    Update layer0.test.js

--- tests/layer0.test.js
+++ tests/layer0.test.js
@@ -1,6 +1,7 @@
 const { describe, test, expect } = require("@jest/globals"); 
 const { randomBytes } = require("crypto");
 const { addLayer0, removeLayer0 } = require("../src/layer0");
+const { messages } = require("./testData");
 
 //65534
 describe("layer0Pack", () => {
@@ -12,5 +13,12 @@ describe("layer0Pack", () => {
       expect(removeLayer0(addLayer0(pkg0))).toStrictEqual(pkg0);
       i = i === 0 ? 1 : i * 2;
     }
+  });
+
+
+  messages.forEach((message) => {
+    test("Test Package", () => {
+      expect(removeLayer0(addLayer0(message))).toStrictEqual(message);
+    });
   });
 });
