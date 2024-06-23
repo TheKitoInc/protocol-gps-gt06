aa25ba96186f7afc838ba16e04406f2827fa9f11
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2024-06-22 21:49:12 -0300

    Create index.test.js

---
+++ tests/protocols/index.test.js
@@ -0,0 +1,9 @@
+const { describe, test, expect } = require("@jest/globals"); 
+const { parse } = require("../../");
+const { messages } = require("../testData");
+
+messages.forEach((message) => {
+  test("Test Package", () => {
+    expect(typeof parse(message).object === "object").toBe(true);
+  });
+});
