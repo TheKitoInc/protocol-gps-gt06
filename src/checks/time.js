027be7977e6a8bb9b34c804da842cdc751b8ce0c
TheKito <11139894+TheKitoInc@users.noreply.github.com>
2024-07-06 09:01:11 -0300

    Create time.js

---
+++ src/checks/time.js
@@ -0,0 +1,20 @@
+const { tryInteger } = require("./types");
+
+module.exports.tryHour = function (value) {
+  tryInteger(value);
+  if (value < 0 || value > 23) {
+    throw new Error("Hour out of range: " + value);
+  }
+};
+module.exports.tryMinute = function (value) {
+  tryInteger(value);
+  if (value < 0 || value > 59) {
+    throw new Error("Minute out of range: " + value);
+  }
+};
+module.exports.trySecond = function (value) {
+  tryInteger(value);
+  if (value < 0 || value > 59) {
+    throw new Error("Second out of range: " + value);
+  }
+};
