07b049ccb05704d252e6f5d62c3911fefd25db57
TheKito <11139894+TheKitoInc@users.noreply.github.com>
2024-07-06 14:26:18 -0300

    Update dateTime.js

--- src/tables/dateTime.js
+++ src/tables/dateTime.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { tryYearShort, tryMonth, tryDay, tryYear } = require("../checks/date");
+const { tryHour, tryMinute, trySecond } = require("../checks/time");
 const { parserPackageComponents } = require("../common");
 
 module.exports.parse = function (buffer) {
@@ -10,12 +12,12 @@ module.exports.parse = function (buffer) {
 
   return {
     timeStamp: {
-      year: year.readUInt8() + 2000,
-      month: month.readUInt8(),
-      day: day.readUInt8(),
-      hours: hours.readUInt8(),
-      minutes: minutes.readUInt8(),
-      seconds: seconds.readUInt8(),
+      year: tryYearShort(year.readUInt8()) + 2000,
+      month: tryMonth(month.readUInt8()),
+      day: tryDay(day.readUInt8()),
+      hours: tryHour(hours.readUInt8()),
+      minutes: tryMinute(minutes.readUInt8()),
+      seconds: trySecond(seconds.readUInt8()),
     },
   };
 };
@@ -23,12 +25,12 @@ module.exports.parse = function (buffer) {
 module.exports.response = function (year, month, day, hours, minutes, seconds) {
   const buffer = Buffer.alloc(6);
 
-  buffer.writeUint8(year - 2000, 0);
-  buffer.writeUint8(month, 1);
-  buffer.writeUint8(day, 2);
-  buffer.writeUint8(hours, 3);
-  buffer.writeUint8(minutes, 4);
-  buffer.writeUint8(seconds, 5);
+  buffer.writeUint8(tryYear(year) - 2000, 0);
+  buffer.writeUint8(tryMonth(month), 1);
+  buffer.writeUint8(tryDay(day), 2);
+  buffer.writeUint8(tryHour(hours), 3);
+  buffer.writeUint8(tryMinute(minutes), 4);
+  buffer.writeUint8(trySecond(seconds), 5);
 
   return buffer;
 };
